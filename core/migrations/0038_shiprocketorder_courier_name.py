90d0f20d9d746e6fd8650486d7bafac70415f334
ramchand2009 <ramchand2009@gmail.com>
2026-05-30 12:23:34 +0530

    Keep courier migration compatible

---
+++ core/migrations/0038_shiprocketorder_courier_name.py
@@ -0,0 +1,12 @@
+# Generated manually to keep production migration history stable.
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0037_webpushsubscription"),
+    ]
+
+    operations = []
